5f39f7f5831af161986619ab0255d1ab61b569b3
husnu <husnu.tapan@gmail.com>
2016-05-20 14:47:40 +0300

    Initial commit

---
+++ dev/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+       <input type="text" class="{{'blue'}}" value="{{onTest()}}">
+       
+    `,
+})
+export class AppComponent {
+    onTest() {
+        return 1 == 1;
+    }
+}
